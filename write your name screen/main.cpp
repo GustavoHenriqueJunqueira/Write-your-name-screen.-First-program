4c88061b34b3c85c4247f93118241023cd98e502
GustavoHenriqueJunqueira <gustavohenriquejunqueira@hotmail.com>
2016-05-22 19:58:52 -0300

    Write your name screen
    basic program , enter your name and also the age and appear on the
    screen

---
+++ write your name screen/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  nome na tela
+//
+//  Created by Gustavo Henrique Furtado Junqueira on 31/03/16.
+//  Copyright © 2016 Gustavo Henrique Furtado Junqueira. All rights reserved.
+//
+//basic program to input your name, and display on the screen
+
+#include <iostream>
+using namespace std;
+int main (){
+    int age;
+    string nome;
+    
+cout<<"write your name:\n";
+    cin>> nome;
+cout<<"write your age:\n";
+    cin>>age;
+cout<<"The name is:" <<nome<< " and age : "<<age;
+    cout<<"\n";
+   
+    return 0;
+    
+}
+
